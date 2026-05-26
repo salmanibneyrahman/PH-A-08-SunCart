d1fcdfda84615c415ca465ff0576d6adf5d9941a
salmanibneyrahman <darazbks@gmail.com>
2026-05-26 15:29:28 +0600

    all the package installed with better auth & mongodb

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,7 @@
-import { betterAuth } from "better-auth";
+import { betterAuth, encodeAsync } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { setPassword } from "better-auth/api";
 
 const client = new MongoClient("process.env.AUTH_DB_URI");
 const db = client.db();
@@ -10,4 +11,18 @@ export const auth = betterAuth({
         // Optional: if you don't provide a client, database transactions won't be enabled.
         client
     }),
+
+
+emailAndPassword: {
+    enabled: true,
+},
+socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
+        }, 
+    },
+
 });
+
+
