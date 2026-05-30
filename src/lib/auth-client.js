6933d31be2f650322799a86d0a3609ea23c1160f
salmanibneyrahman <darazbks@gmail.com>
2026-05-31 00:51:27 +0600

    auth client js modified

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,8 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: typeof window !== "undefined" ? window.location.origin : "http://localhost:3000"
 });
 
-export const {signin, signup, signout, useSession} = 
-createAuthClient()
+export const { signIn, signUp, signOut, useSession } = authClient;
